4eadb0ac4524e5daf99975940501fa00bbda96a1
Nikita Reshetnikov <Mrnikinik@yahoo.com>
2021-02-08 12:54:39 +0300

    newJunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,23 +1,24 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.junit.Test;
 
-public class CashbackHackServiceTest{
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldProposeOne() {
-        assertEquals(service.remain(999),1);
+        assertEquals(1, service.remain(999));
     }
 
     @Test
     public void shouldProposeFiveHundred() {
-        assertEquals(service.remain(1500),500);
+        assertEquals(500, service.remain(1500));
     }
 
     @Test
     public void shouldProposeNull() {
-        assertEquals(service.remain(2000), 0);
+        assertEquals(0, service.remain(2000));
     }
 }
